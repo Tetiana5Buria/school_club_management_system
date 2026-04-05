a7c77bc17166672da2e684c9dedb176b5713e3ee
Tetiana5Buria <poriniya@gmail.com>
2026-04-05 16:59:19 +0300

    update styles

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--g$eqppbuv*2b#%dk*7%&fg^6eh+giqml9c+dg0tr+ny+5hr!m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'python-project-t0qz.onrender.com']
 
 
