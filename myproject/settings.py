eb6e067d0d1bdbf77a4ccf8015e8bc013db833b1
Tetiana5Buria <poriniya@gmail.com>
2026-04-05 17:28:07 +0300

    fix settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--g$eqppbuv*2b#%dk*7%&fg^6eh+giqml9c+dg0tr+ny+5hr!m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'python-project-t0qz.onrender.com']
 
 
@@ -133,7 +133,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATIC_URL = 'static/'
